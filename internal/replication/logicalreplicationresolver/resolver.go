b97b8f0d78ed55de88c1e59dedab47b8a2e18522
Christoph Engelbert (noctarius) <me@noctarius.com>
2023-03-19 18:33:17 +0100

    Code cleanup and deduplication

--- internal/replication/logicalreplicationresolver/resolver.go
+++ internal/replication/logicalreplicationresolver/resolver.go
@@ -109,79 +109,38 @@ func (l *LogicalReplicationResolver) OnInsertEvent(
 	if !ok {
 		logger.Fatalf("unknown relation ID %d", msg.RelationID)
 	}
-	if rel.Namespace == "_timescaledb_catalog" {
-		if rel.RelationName == "hypertable" {
-			return l.dispatcher.EnqueueTask(func(notificator eventhandler.Notificator) {
-				notificator.NotifySystemCatalogReplicationEventHandler(
-					func(handler eventhandler.SystemCatalogReplicationEventHandler) error {
-						return handler.OnHypertableAddedEvent(msg.RelationID, newValues)
-					},
-				)
-			})
-		} else if rel.RelationName == "chunk" {
-			return l.dispatcher.EnqueueTask(func(notificator eventhandler.Notificator) {
-				notificator.NotifySystemCatalogReplicationEventHandler(
-					func(handler eventhandler.SystemCatalogReplicationEventHandler) error {
-						return handler.OnChunkAddedEvent(msg.RelationID, newValues)
-					},
-				)
-			})
-		}
-	} else {
-		chunk, hypertable := l.resolveChunkAndHypertable(rel.Namespace, rel.RelationName)
-		if hypertable != nil {
-			if hypertable.IsCompressedTable() {
-				uncompressedHypertable := l.systemCatalog.FindHypertableByCompressedHypertableId(hypertable.Id())
-				logger.Printf(
-					"COMPRESSION EVENT %s.%s FOR CHUNK %s.%s", uncompressedHypertable.SchemaName(),
-					uncompressedHypertable.HypertableName(), rel.Namespace, rel.RelationName,
-				)
 
-				if !l.genCompressionEvent {
-					return nil
-				}
+	if l.isHypertableEvent(rel) {
+		return l.onHypertableInsertEvent(msg, newValues)
+	}
 
-				return l.dispatcher.EnqueueTask(func(notificator eventhandler.Notificator) {
-					notificator.NotifyCompressionReplicationEventHandler(
-						func(handler eventhandler.CompressionReplicationEventHandler) error {
-							return handler.OnChunkCompressedEvent(xld, hypertable, chunk)
-						},
-					)
-				})
-			} else {
-				if !l.genInsertEvent {
-					return nil
-				}
+	if l.isChunkEvent(rel) {
+		return l.onChunkInsertEvent(msg, newValues)
+	}
 
-				enqueue := func() error {
-					return l.dispatcher.EnqueueTask(func(notificator eventhandler.Notificator) {
-						notificator.NotifyHypertableReplicationEventHandler(
-							func(handler eventhandler.HypertableReplicationEventHandler) error {
-								return handler.OnInsertEvent(xld, hypertable, chunk, newValues)
-							},
-						)
-					})
-				}
-
-				if l.isSnapshotting(chunk) {
-					queue := l.eventQueues[chunk.CanonicalName()]
-					if ok := queue.push(func(snapshot pglogrepl.LSN) error {
-						if xld.ServerWALEnd < snapshot {
-							return nil
-						}
-						return enqueue()
-					}); ok {
-						return nil
-					}
-				}
-
-				if chunk.CompressedChunkId() != nil && !chunk.IsPartiallyCompressed() {
-					return nil
-				}
-				return enqueue()
+	chunk, hypertable := l.resolveChunkAndHypertable(rel.Namespace, rel.RelationName)
+	if hypertable != nil {
+		if hypertable.IsCompressedTable() {
+			if err := l.onChunkCompressionEvent(xld, hypertable, rel, chunk); err != nil {
+				return err
 			}
 		}
+
+		if !l.genInsertEvent {
+			return nil
+		}
+
+		return l.enqueueOrExecute(chunk, xld, func() error {
+			return l.dispatcher.EnqueueTask(func(notificator eventhandler.Notificator) {
+				notificator.NotifyHypertableReplicationEventHandler(
+					func(handler eventhandler.HypertableReplicationEventHandler) error {
+						return handler.OnInsertEvent(xld, hypertable, chunk, newValues)
+					},
+				)
+			})
+		})
 	}
+
 	return nil
 }
 
@@ -192,56 +151,31 @@ func (l *LogicalReplicationResolver) OnUpdateEvent(
 	if !ok {
 		logger.Fatalf("unknown relation ID %d", msg.RelationID)
 	}
-	if rel.Namespace == "_timescaledb_catalog" {
-		if rel.RelationName == "hypertable" {
-			return l.dispatcher.EnqueueTask(func(notificator eventhandler.Notificator) {
-				notificator.NotifySystemCatalogReplicationEventHandler(
-					func(handler eventhandler.SystemCatalogReplicationEventHandler) error {
-						return handler.OnHypertableUpdatedEvent(msg.RelationID, oldValues, newValues)
-					},
-				)
-			})
-		} else if rel.RelationName == "chunk" {
-			return l.dispatcher.EnqueueTask(func(notificator eventhandler.Notificator) {
-				notificator.NotifySystemCatalogReplicationEventHandler(
-					func(handler eventhandler.SystemCatalogReplicationEventHandler) error {
-						return handler.OnChunkUpdatedEvent(msg.RelationID, oldValues, newValues)
-					},
-				)
-			})
-		}
-	} else {
-		if !l.genUpdateEvent {
-			return nil
-		}
-
-		chunk, hypertable := l.resolveChunkAndHypertable(rel.Namespace, rel.RelationName)
-		if hypertable != nil {
-			enqueue := func() error {
-				return l.dispatcher.EnqueueTask(func(notificator eventhandler.Notificator) {
-					notificator.NotifyHypertableReplicationEventHandler(
-						func(handler eventhandler.HypertableReplicationEventHandler) error {
-							return handler.OnUpdateEvent(xld, hypertable, chunk, oldValues, newValues)
-						},
-					)
-				})
-			}
-
-			if l.isSnapshotting(chunk) {
-				queue := l.eventQueues[chunk.CanonicalName()]
-				if ok := queue.push(func(snapshot pglogrepl.LSN) error {
-					if xld.ServerWALEnd < snapshot {
-						return nil
-					}
-					return enqueue()
-				}); ok {
-					return nil
-				}
-			}
-
-			return enqueue()
-		}
+	if l.isHypertableEvent(rel) {
+		return l.onHypertableUpdateEvent(msg, oldValues, newValues)
 	}
+
+	if l.isChunkEvent(rel) {
+		return l.onChunkUpdateEvent(msg, oldValues, newValues)
+	}
+
+	if !l.genUpdateEvent {
+		return nil
+	}
+
+	chunk, hypertable := l.resolveChunkAndHypertable(rel.Namespace, rel.RelationName)
+	if hypertable != nil {
+		return l.enqueueOrExecute(chunk, xld, func() error {
+			return l.dispatcher.EnqueueTask(func(notificator eventhandler.Notificator) {
+				notificator.NotifyHypertableReplicationEventHandler(
+					func(handler eventhandler.HypertableReplicationEventHandler) error {
+						return handler.OnUpdateEvent(xld, hypertable, chunk, oldValues, newValues)
+					},
+				)
+			})
+		})
+	}
+
 	return nil
 }
 
@@ -252,82 +186,32 @@ func (l *LogicalReplicationResolver) OnDeleteEvent(
 	if !ok {
 		logger.Fatalf("unknown relation ID %d", msg.RelationID)
 	}
-	if rel.Namespace == "_timescaledb_catalog" {
-		if rel.RelationName == "hypertable" {
-			return l.dispatcher.EnqueueTask(func(notificator eventhandler.Notificator) {
-				notificator.NotifySystemCatalogReplicationEventHandler(
-					func(handler eventhandler.SystemCatalogReplicationEventHandler) error {
-						return handler.OnHypertableDeletedEvent(msg.RelationID, oldValues)
-					},
-				)
-			})
-		} else if rel.RelationName == "chunk" {
-			if id, ok := oldValues["id"].(int32); ok {
-				chunk := l.systemCatalog.FindChunkById(id)
-				if chunk.IsCompressed() {
-					hypertableId := chunk.HypertableId()
-					uncompressedHypertable := l.systemCatalog.FindHypertableByCompressedHypertableId(hypertableId)
-					logger.Printf(
-						"DECOMPRESSION EVENT %s.%s FOR CHUNK %s.%s", uncompressedHypertable.SchemaName(),
-						uncompressedHypertable.HypertableName(), chunk.SchemaName(), chunk.TableName(),
-					)
 
-					if !l.genDecompressionEvent {
-						return nil
-					}
-
-					if err := l.dispatcher.EnqueueTask(func(notificator eventhandler.Notificator) {
-						notificator.NotifyCompressionReplicationEventHandler(
-							func(handler eventhandler.CompressionReplicationEventHandler) error {
-								return handler.OnChunkDecompressedEvent(xld, uncompressedHypertable, chunk)
-							},
-						)
-					}); err != nil {
-						return err
-					}
-				}
-			}
-
-			return l.dispatcher.EnqueueTask(func(notificator eventhandler.Notificator) {
-				notificator.NotifySystemCatalogReplicationEventHandler(
-					func(handler eventhandler.SystemCatalogReplicationEventHandler) error {
-						return handler.OnChunkDeletedEvent(msg.RelationID, oldValues)
-					},
-				)
-			})
-		}
-	} else {
-		if !l.genUpdateEvent {
-			return nil
-		}
-
-		chunk, hypertable := l.resolveChunkAndHypertable(rel.Namespace, rel.RelationName)
-		if hypertable != nil {
-			enqueue := func() error {
-				return l.dispatcher.EnqueueTask(func(notificator eventhandler.Notificator) {
-					notificator.NotifyHypertableReplicationEventHandler(
-						func(handler eventhandler.HypertableReplicationEventHandler) error {
-							return handler.OnDeleteEvent(xld, hypertable, chunk, oldValues)
-						},
-					)
-				})
-			}
-
-			if l.isSnapshotting(chunk) {
-				queue := l.eventQueues[chunk.CanonicalName()]
-				if ok := queue.push(func(snapshot pglogrepl.LSN) error {
-					if xld.ServerWALEnd < snapshot {
-						return nil
-					}
-					return enqueue()
-				}); ok {
-					return nil
-				}
-			}
-
-			return enqueue()
-		}
+	if l.isHypertableEvent(rel) {
+		return l.onHypertableDeleteEvent(msg, oldValues)
 	}
+
+	if l.isChunkEvent(rel) {
+		return l.onChunkDeleteEvent(xld, msg, oldValues)
+	}
+
+	if !l.genUpdateEvent {
+		return nil
+	}
+
+	chunk, hypertable := l.resolveChunkAndHypertable(rel.Namespace, rel.RelationName)
+	if hypertable != nil {
+		return l.enqueueOrExecute(chunk, xld, func() error {
+			return l.dispatcher.EnqueueTask(func(notificator eventhandler.Notificator) {
+				notificator.NotifyHypertableReplicationEventHandler(
+					func(handler eventhandler.HypertableReplicationEventHandler) error {
+						return handler.OnDeleteEvent(xld, hypertable, chunk, oldValues)
+					},
+				)
+			})
+		})
+	}
+
 	return nil
 }
 
@@ -350,6 +234,159 @@ func (l *LogicalReplicationResolver) OnOriginEvent(xld pglogrepl.XLogData, msg *
 	//TODO implement me
 	logger.Printf("Origin: %+v", msg)
 	return nil
+}
+
+func (l *LogicalReplicationResolver) onHypertableInsertEvent(
+	msg *pglogrepl.InsertMessage, newValues map[string]any) error {
+
+	return l.dispatcher.EnqueueTask(func(notificator eventhandler.Notificator) {
+		notificator.NotifySystemCatalogReplicationEventHandler(
+			func(handler eventhandler.SystemCatalogReplicationEventHandler) error {
+				return handler.OnHypertableAddedEvent(msg.RelationID, newValues)
+			},
+		)
+	})
+}
+
+func (l *LogicalReplicationResolver) onChunkInsertEvent(
+	msg *pglogrepl.InsertMessage, newValues map[string]any) error {
+
+	return l.dispatcher.EnqueueTask(func(notificator eventhandler.Notificator) {
+		notificator.NotifySystemCatalogReplicationEventHandler(
+			func(handler eventhandler.SystemCatalogReplicationEventHandler) error {
+				return handler.OnChunkAddedEvent(msg.RelationID, newValues)
+			},
+		)
+	})
+}
+
+func (l *LogicalReplicationResolver) onHypertableUpdateEvent(msg *pglogrepl.UpdateMessage,
+	oldValues map[string]any, newValues map[string]any) error {
+
+	return l.dispatcher.EnqueueTask(func(notificator eventhandler.Notificator) {
+		notificator.NotifySystemCatalogReplicationEventHandler(
+			func(handler eventhandler.SystemCatalogReplicationEventHandler) error {
+				return handler.OnHypertableUpdatedEvent(msg.RelationID, oldValues, newValues)
+			},
+		)
+	})
+}
+
+func (l *LogicalReplicationResolver) onChunkUpdateEvent(msg *pglogrepl.UpdateMessage,
+	oldValues map[string]any, newValues map[string]any) error {
+
+	return l.dispatcher.EnqueueTask(func(notificator eventhandler.Notificator) {
+		notificator.NotifySystemCatalogReplicationEventHandler(
+			func(handler eventhandler.SystemCatalogReplicationEventHandler) error {
+				return handler.OnChunkUpdatedEvent(msg.RelationID, oldValues, newValues)
+			},
+		)
+	})
+}
+
+func (l *LogicalReplicationResolver) onHypertableDeleteEvent(
+	msg *pglogrepl.DeleteMessage, oldValues map[string]any) error {
+
+	return l.dispatcher.EnqueueTask(func(notificator eventhandler.Notificator) {
+		notificator.NotifySystemCatalogReplicationEventHandler(
+			func(handler eventhandler.SystemCatalogReplicationEventHandler) error {
+				return handler.OnHypertableDeletedEvent(msg.RelationID, oldValues)
+			},
+		)
+	})
+}
+
+func (l *LogicalReplicationResolver) onChunkDeleteEvent(xld pglogrepl.XLogData,
+	msg *pglogrepl.DeleteMessage, oldValues map[string]any) error {
+
+	if id, ok := oldValues["id"].(int32); ok {
+		chunk := l.systemCatalog.FindChunkById(id)
+		if chunk.IsCompressed() {
+			if err := l.onChunkDecompressionEvent(xld, chunk); err != nil {
+				return err
+			}
+		}
+	}
+
+	return l.dispatcher.EnqueueTask(func(notificator eventhandler.Notificator) {
+		notificator.NotifySystemCatalogReplicationEventHandler(
+			func(handler eventhandler.SystemCatalogReplicationEventHandler) error {
+				return handler.OnChunkDeletedEvent(msg.RelationID, oldValues)
+			},
+		)
+	})
+}
+
+func (l *LogicalReplicationResolver) onChunkCompressionEvent(xld pglogrepl.XLogData,
+	hypertable *model.Hypertable, rel *pglogrepl.RelationMessage, chunk *model.Chunk) error {
+
+	uncompressedHypertable := l.systemCatalog.FindHypertableByCompressedHypertableId(hypertable.Id())
+	logger.Printf(
+		"COMPRESSION EVENT %s.%s FOR CHUNK %s.%s", uncompressedHypertable.SchemaName(),
+		uncompressedHypertable.HypertableName(), rel.Namespace, rel.RelationName,
+	)
+
+	if !l.genCompressionEvent {
+		return nil
+	}
+
+	return l.dispatcher.EnqueueTask(func(notificator eventhandler.Notificator) {
+		notificator.NotifyCompressionReplicationEventHandler(
+			func(handler eventhandler.CompressionReplicationEventHandler) error {
+				return handler.OnChunkCompressedEvent(xld, hypertable, chunk)
+			},
+		)
+	})
+}
+
+func (l *LogicalReplicationResolver) onChunkDecompressionEvent(xld pglogrepl.XLogData, chunk *model.Chunk) error {
+	hypertableId := chunk.HypertableId()
+	uncompressedHypertable := l.systemCatalog.FindHypertableByCompressedHypertableId(hypertableId)
+	logger.Printf(
+		"DECOMPRESSION EVENT %s.%s FOR CHUNK %s.%s", uncompressedHypertable.SchemaName(),
+		uncompressedHypertable.HypertableName(), chunk.SchemaName(), chunk.TableName(),
+	)
+
+	if !l.genDecompressionEvent {
+		return nil
+	}
+
+	if err := l.dispatcher.EnqueueTask(func(notificator eventhandler.Notificator) {
+		notificator.NotifyCompressionReplicationEventHandler(
+			func(handler eventhandler.CompressionReplicationEventHandler) error {
+				return handler.OnChunkDecompressedEvent(xld, uncompressedHypertable, chunk)
+			},
+		)
+	}); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (l *LogicalReplicationResolver) isHypertableEvent(relation *pglogrepl.RelationMessage) bool {
+	return relation.Namespace == "_timescaledb_catalog" && relation.RelationName == "hypertable"
+}
+
+func (l *LogicalReplicationResolver) isChunkEvent(relation *pglogrepl.RelationMessage) bool {
+	return relation.Namespace == "_timescaledb_catalog" && relation.RelationName == "chunk"
+}
+
+func (l *LogicalReplicationResolver) enqueueOrExecute(chunk *model.Chunk,
+	xld pglogrepl.XLogData, fn func() error) error {
+
+	if l.isSnapshotting(chunk) {
+		queue := l.eventQueues[chunk.CanonicalName()]
+		if ok := queue.push(func(snapshot pglogrepl.LSN) error {
+			if xld.ServerWALEnd < snapshot {
+				return nil
+			}
+			return fn()
+		}); ok {
+			return nil
+		}
+	}
+
+	return fn()
 }
 
 func (l *LogicalReplicationResolver) isSnapshotting(chunk *model.Chunk) bool {
