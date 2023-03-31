b3f5608f0b82baab9b27bc8982763f7ca1273ee9
Christoph Engelbert (noctarius) <me@noctarius.com>
2023-03-31 13:35:24 +0200

    Updated and finished Column implementation

--- internal/systemcatalog/model/column.go
+++ internal/systemcatalog/model/column.go
@@ -1,21 +1,23 @@
 package model
 
+import "fmt"
+
 type Column struct {
 	name         string
 	dataType     uint32
 	typeName     string
 	nullable     bool
-	identity     bool
+	primaryKey   bool
 	defaultValue *string
 }
 
-func NewColumn(name string, dataType uint32, typeName string, nullable, identity bool, defaultValue *string) Column {
+func NewColumn(name string, dataType uint32, typeName string, nullable, primaryKey bool, defaultValue *string) Column {
 	return Column{
 		name:         name,
 		dataType:     dataType,
 		typeName:     typeName,
 		nullable:     nullable,
-		identity:     identity,
+		primaryKey:   primaryKey,
 		defaultValue: defaultValue,
 	}
 }
@@ -36,8 +38,8 @@ func (c Column) Nullable() bool {
 	return c.nullable
 }
 
-func (c Column) Identify() bool {
-	return c.identity
+func (c Column) PrimaryKey() bool {
+	return c.primaryKey
 }
 
 func (c Column) DefaultValue() *string {
@@ -49,7 +51,7 @@ func (c Column) equals(other Column) bool {
 		c.typeName == other.typeName &&
 		c.dataType == other.dataType &&
 		c.nullable == other.nullable &&
-		c.identity == other.identity &&
+		c.primaryKey == other.primaryKey &&
 		((c.defaultValue == nil && other.defaultValue == nil) ||
 			(c.defaultValue != nil && other.defaultValue != nil && *c.defaultValue == *other.defaultValue))
 }
@@ -58,13 +60,39 @@ func (c Column) equalsExceptName(other Column) bool {
 	return c.typeName == other.typeName &&
 		c.dataType == other.dataType &&
 		c.nullable == other.nullable &&
-		c.identity == other.identity &&
+		c.primaryKey == other.primaryKey &&
 		((c.defaultValue == nil && other.defaultValue == nil) ||
 			(c.defaultValue != nil && other.defaultValue != nil && *c.defaultValue == *other.defaultValue))
 }
 
 func (c Column) differences(new Column) map[string]string {
 	differences := make(map[string]string, 0)
-	// TODO
+	if c.name != new.name {
+		differences["name"] = fmt.Sprintf("%s=>%s", c.name, new.name)
+	}
+	if c.dataType != new.dataType {
+		differences["dataType"] = fmt.Sprintf("%d=>%d", c.dataType, new.dataType)
+	}
+	if c.typeName != new.typeName {
+		differences["typeName"] = fmt.Sprintf("%s=>%s", c.typeName, new.typeName)
+	}
+	if c.nullable != new.nullable {
+		differences["nullable"] = fmt.Sprintf("%t=>%t", c.nullable, new.nullable)
+	}
+	if c.primaryKey != new.primaryKey {
+		differences["primaryKey"] = fmt.Sprintf("%t=>%t", c.primaryKey, new.primaryKey)
+	}
+	if (c.defaultValue == nil && new.defaultValue != nil) ||
+		(c.defaultValue != nil && new.defaultValue == nil) {
+		o := "<nil>"
+		if c.defaultValue != nil {
+			o = fmt.Sprintf("%s", *c.defaultValue)
+		}
+		n := "<nil>"
+		if new.defaultValue != nil {
+			n = fmt.Sprintf("%s", *new.defaultValue)
+		}
+		differences["defaultValue"] = fmt.Sprintf("%s=>%s", o, n)
+	}
 	return differences
 }
