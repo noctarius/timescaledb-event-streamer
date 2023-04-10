0c5022a6fd66724cf6aac60e8c7c03b524629ee3
Christoph Engelbert (noctarius) <me@noctarius.com>
2023-04-10 12:42:38 +0200

    Fixed nil pointer in AdaptError when no error happened

--- internal/supporting/errors.go
+++ internal/supporting/errors.go
@@ -3,6 +3,9 @@ package supporting
 import "github.com/urfave/cli"
 
 func AdaptError(err error, exitCode int) *cli.ExitError {
+	if err == nil {
+		return nil
+	}
 	if e, ok := err.(*cli.ExitError); ok {
 		return e
 	}
