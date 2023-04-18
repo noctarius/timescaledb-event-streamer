2911d54c75cc1caa831c1f21b66df7795642d41c
Christoph Engelbert (noctarius) <me@noctarius.com>
2023-04-18 09:08:50 +0200

    Reworked the stdout sink to only provide the values as json objects with a newline at the end

--- internal/eventing/sinks/stdout/stdout.go
+++ internal/eventing/sinks/stdout/stdout.go
@@ -2,7 +2,6 @@ package stdout
 
 import (
 	"encoding/json"
-	"fmt"
 	spiconfig "github.com/noctarius/timescaledb-event-streamer/spi/config"
 	"github.com/noctarius/timescaledb-event-streamer/spi/schema"
 	"github.com/noctarius/timescaledb-event-streamer/spi/sink"
@@ -14,19 +13,16 @@ func init() {
 	sink.RegisterSink(spiconfig.Stdout, newStdoutSink)
 }
 
-type stdoutSink struct {
-}
-
 func newStdoutSink(_ *spiconfig.Config) (sink.Sink, error) {
-	return &stdoutSink{}, nil
-}
-
-func (s *stdoutSink) Emit(_ time.Time, topicName string, _, envelope schema.Struct) error {
-	delete(envelope, "schema")
-	data, err := json.Marshal(envelope)
-	if err != nil {
-		return err
-	}
-	_, err = os.Stdout.WriteString(fmt.Sprintf("===> /%s: \t%s\n", topicName, string(data)))
-	return err
+	return sink.SinkFunc(
+		func(context *sink.Context, timestamp time.Time, topicName string, key, envelope schema.Struct) error {
+			delete(envelope, "schema")
+			data, err := json.Marshal(envelope)
+			if err != nil {
+				return err
+			}
+			_, err = os.Stdout.WriteString(string(data))
+			return err
+		},
+	), nil
 }
