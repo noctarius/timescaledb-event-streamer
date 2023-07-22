d16f06dc0d39312229b56731ace75a9a1b7675c7
Christoph Engelbert (noctarius) <me@noctarius.com>
2023-07-22 13:01:08 +0200

    Added build constraints for the plugin system since it's only available on freebsd, linux, and macos

--- spi/plugins/plugins.go
+++ spi/plugins/plugins.go
@@ -1,3 +1,5 @@
+//go:build linux || freebsd || darwin
+
 package plugins
 
 import (
---
+++ spi/plugins/plugins_noop.go
@@ -0,0 +1,16 @@
+//go:build !linux && !freebsd && !darwin
+
+package plugins
+
+import (
+	"github.com/go-errors/errors"
+	"github.com/noctarius/timescaledb-event-streamer/spi/config"
+	"runtime"
+)
+
+func LoadPlugins(config *config.Config) error {
+	if len(config.Plugins) > 0 {
+		return errors.Errorf("Plugins aren't supported on %s, but plugins are defined in config", runtime.GOOS)
+	}
+	return nil
+}
